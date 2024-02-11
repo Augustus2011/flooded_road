66c521b4a4225acec5dba19cead59db6b287d7c7
Augustus2011 <kun.kerdthaisong@gmail.com>
2024-02-11 23:12:45 +0700

    to py and crete gitignore

---
+++ clip_as_a_searcher.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from transformers import CLIPProcessor, CLIPModel
+import glob
+from read_img import read_from_url
+import torch
+from PIL import Image
+
+print(read_from_url("https://storage.googleapis.com/traffy_public_bucket/attachment/2022-06/182f43ac1d9c4a09d1ed8009c851f4c5f4460d3d.jpg")) #https://storage.googleapis.com/traffy_public_bucket/attachment/2022-06/182f43ac1d9c4a09d1ed8009c851f4c5f4460d3d.jpg
+
+
+def run(image:Image)->torch.Tensor:
+        inputs =processor(
+            text=["manhole","manhole cover","sink hole","road","not road"], #text=["flood", "flooded road", "water on floor", "road", "no water","dried road"],
+            images=image,
+            return_tensors="pt",
+            padding=True
+        )
+        with torch.no_grad():
+            outputs =model(**inputs)
+            logits_per_image = outputs.logits_per_image
+            probs = logits_per_image.softmax(dim=1)
+        return probs
+
+
+
+if __name__ =="__main__":
+    df=pd.read_csv("/Users/kunkerdthaisong/cils/bangkok_traffy.csv")
+    df=df.dropna(subset=["type"])
+    df=df[df["type"].str.contains("ท่อ")] #30288 rows
+
+    model = CLIPModel.from_pretrained("openai/clip-vit-large-patch14")
+    processor = CLIPProcessor.from_pretrained("openai/clip-vit-large-patch14")
+
+    count=0 #handle error manually  when url is nan
+    for i in df["photo"][0:]:
+        img=read_from_url(i)
+        probs=run(img)
+        if probs.squeeze(0)[:3].sum()>0.5 and probs.squeeze(0)[-1:].sum()<0.2: #probs.squeeze(0)[1] >0.3 and probs.squeeze(0)[:3].sum()>0.55 and probs.squeeze(0)[-2:].sum()<0.25:
+            img.save(f"/Users/kunkerdthaisong/cils/images2/{count}.jpg")
+        count+=1
--- read_img.py
+++ read_img.py
@@ -4,20 +4,19 @@ from io import BytesIO
 
 
 #read image from url
-def read_and_save_image(url, save_path):
+def read_from_url(url:str)->Image:
 
     response = requests.get(url)
     
     if response.status_code == 200:
 
-        image = Image.open(BytesIO(response.content))
-        
-        image.save(save_path)
-        print(f"Image saved successfully at: {save_path}")
+        image = Image.open(BytesIO(response.content)) #got PIL.Image object
+        return image
     else:
         print(f"Failed to download image from URL: {url}")
 
-url = "https://example.com/image.jpg"
-save_path = "image.jpg"
 
-read_and_save_image(url, save_path)
+
+#url = "https://example.com/image.jpg"
+#save_path = "image.jpg"
+#read_and_save_image(url, save_path)
